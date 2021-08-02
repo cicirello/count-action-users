4a6f2750ddff0f56acdf56a1b74cf492b5195b07
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2021-08-02 16:21:02 -0400

    Update ActionUserCounter.py

--- ActionUserCounter.py
+++ ActionUserCounter.py
@@ -28,6 +28,75 @@
 #
 
 import sys
+import json
+import os
+import os.path
+import pathlib
+
+queryTemplate = """search/code?q={0}+path%3A.github%2Fworkflows+language%3AYAML"""
+
+def splitActionOwnerName(action) :
+    """Takes the name of an action that may include both owner and action
+    name, and splits it.
+
+    Keyword arguments:
+    action - Name of action, possibly including both owner and name.
+    """
+    s = action.split("/")
+    return (s[0], s[1]) if len(s) > 1 else ("", s[0])
+
+def executeCommand(arguments) :
+    """Execute a subprocess and return result and exit code.
+    
+    Keyword arguments:
+    arguments - The arguments for the command.
+    """
+    result = subprocess.run(
+        arguments,
+        stdout=subprocess.PIPE,
+        universal_newlines=True
+        )
+    return result.stdout.strip(), result.returncode
+
+def executeQuery(owner, actionName, failOnError) :
+    """Executes a query for an owner actionName combination.
+
+    Keyword arguments:
+    owner - The owner of the action, which may be the empty string for this query.
+    actionName - The name of the action.
+    failOnError - Pass True to fail the workflow if an error occurs.
+    """
+    q = owner + "+" + actionName if len(owner) > 0 else actionName
+    result, exitCode = executeCommand(["gh", "api", queryTemplate.format(q)])
+    if exitCode != 0 :
+        print("Error: An error with code", exitCode, "occurred during GitHub API query.")
+        print(result)
+        print("::set-output name=exit-code::" + str(exitCode))
+        exit(exitCode if failOnError else 0)
+    result = json.loads(result)
+    if "total_count" in result :
+        return result["total_count"]
+    else :
+        print("Error: total_count missing from GitHub API query result")
+        exitCode = 1
+        print("::set-output name=exit-code::" + str(exitCode))
+        exit(exitCode if failOnError else 0)
+
+def collectRepoCounts(actionList, failOnError) :
+    """Executes all necessary queries to gather the user counts of
+    all actions in the actionList. Returns a map from action name to
+    associated count.
+
+    Keyword arguments:
+    actionList - A list of actions to report usage on
+    failOnError - Pass True to fail the workflow if an error occurs
+    """
+    countMap = {}
+    for action in actionList :
+        owner, actionName = splitActionOwnerName(action)
+        count = executeQuery(owner, actionName, failOnError)
+        countMap[actionName] = count
+    return countMap
 
 if __name__ == "__main__" :
     actionList = sys.argv[1].strip().replace(",", " ").split()
@@ -45,8 +114,21 @@ if __name__ == "__main__" :
     logoName = sys.argv[7].strip()
     
     style = sys.argv[8].strip()
-    
+
     exitCode = 0
+    
+    countMap = collectRepoCounts(actionList, failOnError)
+    if len(targetDirectory) > 0 :
+        if not os.path.exists(targetDirectory) :
+            p = pathlib.Path(targetDirectory)
+            os.umask(0)
+            p.mkdir(mode=0o777, parents=True, exist_ok=True)
+        os.chdir(targetDirectory)
+
+    # Iterate over countMap.items() and generate JSON for each
+
+    # Commit and push
+    
     
     print("::set-output name=exit-code::" + str(exitCode))
     
